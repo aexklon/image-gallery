dee0c8ab20b9c79d6b1959b02f5a17c187aa9bf8
alexandre lopes <alexfl.dsg@gmail.com>
2016-12-22 02:42:49 -0200

    search implemented

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,53 +1,92 @@
 'use strict';
 
+// declaring the state so it can be used later
 var state = {};
-function setState(changes, key) {
+
+// function that will be called to change the state
+function setState(changes) {
 	Object.assign(state, changes);
-	ReactDOM.render(React.createElement(Minister, {
-		crumbs: changes[key].crumbs,
-		dirs: changes[key].dirs,
-		files: changes[key].files
+	ReactDOM.render(React.createElement(Outputer, {
+		crumbs: state[state.cwd].crumbs,
+		dirs: state[state.cwd].dirs,
+		files: state.filesFiltered,
+		containerSize: state.containerSize,
+		cardSize: state.cardSize,
+		cardInfo: state.cardInfo
 	}), document.getElementById('minister'));
-	console.log('state', state);
+	//console.log('state', state)
 }
+
+// translate URLs between client and server side
 function translate(url) {
 	return url.replace('http://127.0.0.1:8000', 'http://127.0.0.1:8080/outputer');
 }
+
+// set the API URI based on the browser adress, and translate it to API call
 var fetchPath = ('http://127.0.0.1:8000' + window.location.pathname).replace('/outputer', '');
-console.log(fetchPath);
-fetch(fetchPath).then(function (res) {
+
+// promisse a request to the API
+fetch(fetchPath)
+// parse the body of the response
+.then(function (res) {
 	return res.json();
-}).then(function (value) {
+})
+// Change the state, with the key being the the current page
+// and the value the parsed json
+.then(function (value) {
 	var changes = {};
-	var key = value.crumbs[value.crumbs.length - 1].name;
+	var key = window.location.pathname;
 	changes[key] = value;
-	console.log('changes', key, changes);
-	setState(changes, key);
+	changes.cwd = key;
+	//console.log('changes', key, changes)
+	setState(changes);
+}).then(function () {
+	var changes = {};
+	changes.filesFiltered = state[state.cwd].files;
+	changes.containerSize = localStorage.getItem('containerSize');
+	changes.cardSize = localStorage.getItem('cardSize');
+	changes.cardInfo = false;
+	setState(changes);
 });
 
-var Minister = React.createClass({
-	displayName: 'Minister',
+// Master component
+var Outputer = React.createClass({
+	displayName: 'Outputer',
 
 	render: function render() {
 		var _props = this.props,
 		    crumbs = _props.crumbs,
 		    dirs = _props.dirs,
-		    files = _props.files;
+		    files = _props.files,
+		    filesFiltered = _props.filesFiltered,
+		    containerSize = _props.containerSize,
+		    cardSize = _props.cardSize,
+		    cardInfo = _props.cardInfo;
 
 		return React.createElement(
 			'div',
-			null,
-			React.createElement(Navbar, { crumbs: crumbs }),
+			{ id: 'outputer' },
+			React.createElement(
+				'nav',
+				null,
+				React.createElement(Navbar, { containerSize: containerSize, cardSize: cardSize, cardInfo: cardInfo }),
+				React.createElement(
+					'div',
+					{ className: 'container ' + containerSize },
+					crumbs && crumbs[0] ? React.createElement(Breadcrumbs, { crumbs: crumbs }) : null
+				)
+			),
 			React.createElement(
 				'article',
-				{ className: 'container' },
-				console.log('props', this.props),
-				dirs[0] ? React.createElement(Directories, { dirs: dirs }) : null,
-				files[0] ? React.createElement(Files, { files: files }) : null
+				{ className: 'container ' + containerSize },
+				dirs && dirs[0] ? React.createElement(Directories, { dirs: dirs }) : null,
+				files && files[0] ? React.createElement(Files, { files: files, cardSize: cardSize, cardInfo: cardInfo }) : null
 			)
 		);
 	}
 });
+
+// Displays Breadcrumbs for the current page
 var Breadcrumbs = React.createClass({
 	displayName: 'Breadcrumbs',
 
@@ -71,6 +110,8 @@ var Breadcrumbs = React.createClass({
 		);
 	}
 });
+
+// Displays the sub directories fot the current page
 var Directories = React.createClass({
 	displayName: 'Directories',
 
@@ -90,40 +131,49 @@ var Directories = React.createClass({
 		);
 	}
 });
+
+// Displays the files in the current page, in Cards
 var Files = React.createClass({
 	displayName: 'Files',
 
 	render: function render() {
-		var files = this.props.files;
+		var _props2 = this.props,
+		    files = _props2.files,
+		    cardSize = _props2.cardSize,
+		    cardInfo = _props2.cardInfo;
 
 		return React.createElement(
 			'main',
-			{ className: 'files mt-1' },
+			{ className: 'ot-files mt-1' },
 			files.map(function (file, i) {
-				return React.createElement(Card, { file: file, key: i });
+				return React.createElement(Card, { file: file, key: i, cardSize: cardSize, cardInfo: cardInfo });
 			})
 		);
 	}
 });
 
+// Displays a file as a linked images
 var Card = React.createClass({
 	displayName: 'Card',
 
 	render: function render() {
-		var file = this.props.file;
+		var _props3 = this.props,
+		    file = _props3.file,
+		    cardSize = _props3.cardSize,
+		    cardInfo = _props3.cardInfo;
 
 		return React.createElement(
 			'div',
-			{ className: 'card' },
-			React.createElement(
+			{ className: 'card ' + cardSize },
+			cardInfo ? React.createElement(
 				'div',
-				{ className: 'card-block hidden-xs-up' },
+				{ className: 'card-block' },
 				React.createElement(
 					'h4',
 					{ className: 'card-title' },
 					file.sequence
 				)
-			),
+			) : null,
 			React.createElement(
 				'div',
 				{ className: 'ot-imageWrapper' },
@@ -138,89 +188,162 @@ var Card = React.createClass({
 					React.createElement('i', { className: 'fa fa-link' })
 				)
 			),
-			React.createElement(
+			cardInfo && file.keywords ? React.createElement(
 				'ul',
-				{ className: 'list-group list-group-flush hidden-xs-up' },
-				file.keywords ? file.keywords.map(function (keyword, i) {
+				{ className: 'list-group list-group-flush' },
+				file.keywords.map(function (keyword, i) {
 					return React.createElement(
 						'li',
 						{ className: 'list-group-item', key: i },
 						keyword
 					);
-				}) : null
-			)
+				})
+			) : null
 		);
 	}
 });
+
+// Displays the Navbar
+// Its functionality will be developed in next commits. Right now just the breadcrumbs are dynamic
 var Navbar = React.createClass({
 	displayName: 'Navbar',
 
+	_changeContainerSize: function _changeContainerSize(containerSize, scale) {
+		var sizes = ['container--500', 'container--900', 'container--1140', 'container--full'];
+		var currentSize = sizes.indexOf(containerSize);
+		var finalSize = containerSize;
+		if (scale === "increase" && currentSize < sizes.length - 1) {
+			finalSize = sizes[sizes.indexOf(containerSize) + 1];
+		} else if (scale === "decrease" && currentSize > 0) {
+			finalSize = sizes[sizes.indexOf(containerSize) - 1];
+		}
+		setState({ "containerSize": finalSize });
+		localStorage.setItem('containerSize', finalSize);
+	},
+	_changeCardSize: function _changeCardSize(cardSize, scale) {
+		var sizes = ['card--80', 'card--120', 'card--180', 'card--full'];
+		var currentSize = sizes.indexOf(cardSize);
+		var finalSize = cardSize;
+		if (scale === "increase" && currentSize < sizes.length - 1) {
+			finalSize = sizes[sizes.indexOf(cardSize) + 1];
+		} else if (scale === "decrease" && currentSize > 0) {
+			finalSize = sizes[sizes.indexOf(cardSize) - 1];
+		}
+		setState({ "cardSize": finalSize });
+		localStorage.setItem('cardSize', finalSize);
+	},
+	_changeCardInfo: function _changeCardInfo(cardInfo) {
+		setState({ "cardInfo": !cardInfo });
+	},
+	_search: function _search(kind, input) {
+		switch (kind) {
+			case "including":
+				setState({ "searchIncluding": input });
+				break;
+			case "excluding":
+				setState({ "searchExcluding": input });
+				break;
+		}
+		setTimeout(function () {
+			if (state.searchIncluding) {
+				if (!state.searchExcluding) {
+					setState({ "filesFiltered": lookFor(state.searchIncluding, state[state.cwd].files) });
+				} else {
+					setState({ "filesFiltered": except(state.searchExcluding, lookFor(state.searchIncluding, state[state.cwd].files)) });
+				}
+			} else if (!state.searchIncluding) {
+				setState({ "filesFiltered": state[state.cwd].files });
+			}
+		}, 200);
+		function lookFor(term, files) {
+			var re = new RegExp(term, "i");
+			var filesFiltered = files.filter(function (file) {
+				return file.keywords.map(function (keyword) {
+					return re.test(keyword);
+				}).reduce(function (a, b) {
+					return a + b;
+				});
+			});
+			return filesFiltered;
+		}
+		function except(term, files) {
+			var re = new RegExp(term, "i");
+			var filesFiltered = files.filter(function (file) {
+				return !file.keywords.map(function (keyword) {
+					return re.test(keyword);
+				}).reduce(function (a, b) {
+					return a + b;
+				});
+			});
+			return filesFiltered;
+		}
+	},
 	render: function render() {
-		var crumbs = this.props.crumbs;
+		var _this = this;
+
+		var _props4 = this.props,
+		    containerSize = _props4.containerSize,
+		    cardSize = _props4.cardSize,
+		    cardInfo = _props4.cardInfo;
 
 		return React.createElement(
-			'nav',
-			null,
+			'div',
+			{ className: 'navbar' },
 			React.createElement(
 				'div',
-				{ className: 'navbar' },
+				{ className: 'container ' + containerSize },
+				React.createElement('button', { className: 'navbar-toggler hidden-md-up', type: 'button', 'data-toggle': 'collapse', 'data-target': '#navbarResponsive', 'aria-controls': 'navbarResponsive', 'aria-expanded': 'false', 'aria-label': 'Toggle navigation' }),
 				React.createElement(
 					'div',
-					{ className: 'container' },
-					React.createElement('button', { className: 'navbar-toggler hidden-md-up', type: 'button', 'data-toggle': 'collapse', 'data-target': '#navbarResponsive', 'aria-controls': 'navbarResponsive', 'aria-expanded': 'false', 'aria-label': 'Toggle navigation' }),
+					{ className: 'collapse navbar-toggleable-sm', id: 'navbarResponsive' },
 					React.createElement(
 						'div',
-						{ className: 'collapse navbar-toggleable-sm', id: 'navbarResponsive' },
+						{ className: 'btn-group ot-images', role: 'group', 'aria-label': 'Resize images' },
 						React.createElement(
-							'div',
-							{ className: 'btn-group ot-images', role: 'group', 'aria-label': 'Resize images' },
-							React.createElement(
-								'button',
-								{ type: 'button', className: 'btn btn-secondary' },
-								React.createElement('i', { className: 'fa fa-compress', 'aria-hidden': 'true' })
-							),
-							React.createElement(
-								'button',
-								{ type: 'button', className: 'btn btn-secondary' },
-								React.createElement('i', { className: 'fa fa-expand', 'aria-hidden': 'true' })
-							)
+							'button',
+							{ type: 'button', className: 'btn btn-secondary', onClick: this._changeCardSize.bind(this, cardSize, "decrease") },
+							React.createElement('i', { className: 'fa fa-compress', 'aria-hidden': 'true' })
 						),
 						React.createElement(
-							'div',
-							{ className: 'btn-group ot-container', role: 'group', 'aria-label': 'Resize container' },
-							React.createElement(
-								'button',
-								{ type: 'button', className: 'btn btn-secondary' },
-								React.createElement('i', { className: 'fa fa-mobile', 'aria-hidden': 'true' })
-							),
-							React.createElement(
-								'button',
-								{ type: 'button', className: 'btn btn-secondary' },
-								React.createElement('i', { className: 'fa fa-desktop', 'aria-hidden': 'true' })
-							)
-						),
-						React.createElement(
-							'div',
-							{ className: 'btn-group ot-code', role: 'group', 'aria-label': 'Resize container' },
-							React.createElement(
-								'button',
-								{ type: 'button', className: 'btn btn-secondary' },
-								React.createElement('i', { className: 'fa fa-code', 'aria-hidden': 'true' })
-							)
-						),
-						React.createElement(
-							'form',
-							{ className: 'form-inline float-md-right' },
-							React.createElement('input', { className: 'ot-input form-control', type: 'text', placeholder: 'Search' }),
-							React.createElement('input', { className: 'ot-input form-control', type: 'text', placeholder: 'Exclude' })
+							'button',
+							{ type: 'button', className: 'btn btn-secondary', onClick: this._changeCardSize.bind(this, cardSize, "increase") },
+							React.createElement('i', { className: 'fa fa-expand', 'aria-hidden': 'true' })
 						)
+					),
+					React.createElement(
+						'div',
+						{ className: 'btn-group ot-container', role: 'group', 'aria-label': 'Resize container' },
+						React.createElement(
+							'button',
+							{ type: 'button', className: 'btn btn-secondary', onClick: this._changeContainerSize.bind(this, containerSize, "decrease") },
+							React.createElement('i', { className: 'fa fa-mobile', 'aria-hidden': 'true' })
+						),
+						React.createElement(
+							'button',
+							{ type: 'button', className: 'btn btn-secondary', onClick: this._changeContainerSize.bind(this, containerSize, "increase") },
+							React.createElement('i', { className: 'fa fa-desktop', 'aria-hidden': 'true' })
+						)
+					),
+					React.createElement(
+						'div',
+						{ className: 'btn-group ot-code', role: 'group', 'aria-label': 'Resize container' },
+						React.createElement(
+							'button',
+							{ type: 'button', className: 'btn btn-secondary', onClick: this._changeCardInfo.bind(this, cardInfo) },
+							React.createElement('i', { className: 'fa fa-code', 'aria-hidden': 'true' })
+						)
+					),
+					React.createElement(
+						'form',
+						{ className: 'form-inline float-md-right' },
+						React.createElement('input', { className: 'ot-input form-control', type: 'text', placeholder: 'Search', onChange: function onChange(e) {
+								return _this._search("including", e.target.value);
+							} }),
+						React.createElement('input', { className: 'ot-input form-control', type: 'text', placeholder: 'Exclude', onChange: function onChange(e) {
+								return _this._search("excluding", e.target.value);
+							}, disabled: !state.searchIncluding })
 					)
 				)
-			),
-			React.createElement(
-				'div',
-				{ className: 'container' },
-				crumbs[0] ? React.createElement(Breadcrumbs, { crumbs: crumbs }) : null
 			)
 		);
 	}
--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@
 let state = {}
 
 // function that will be called to change the state
-function setState(changes, key) {
+function setState(changes) {
   Object.assign(state, changes)
 	ReactDOM.render(
-		<Minister
-			crumbs={changes[key].crumbs}
-			dirs={changes[key].dirs}
-			files={changes[key].files}
+		<Outputer
+			crumbs={state[state.cwd].crumbs}
+			dirs={state[state.cwd].dirs}
+			files={state.filesFiltered}
+			containerSize={state.containerSize}
+			cardSize={state.cardSize}
+			cardInfo={state.cardInfo}
 		/>, document.getElementById('minister')
 	)
-	console.log('state', state)
+	//console.log('state', state)
 }
 
 // translate URLs between client and server side
@@ -30,23 +33,36 @@ fetch(fetchPath)
 	// and the value the parsed json
 	.then(value=>{
 		let changes = {}
-		let key = value.crumbs[value.crumbs.length-1].name
+		let key = window.location.pathname
 		changes[key] = value
-		console.log('changes', key, changes)
-		setState(changes, key)
+		changes.cwd = key
+		//console.log('changes', key, changes)
+		setState(changes)
+	})
+	.then(()=>{
+		let changes = {}
+		changes.filesFiltered = state[state.cwd].files
+		changes.containerSize =  localStorage.getItem('containerSize')
+		changes.cardSize = localStorage.getItem('cardSize')
+		changes.cardInfo = false
+		setState(changes)
 	})
 
 // Master component
-const Minister = React.createClass({
+const Outputer = React.createClass({
   render: function() {
-		let {crumbs, dirs, files} = this.props
+		let {crumbs, dirs, files, filesFiltered, containerSize, cardSize, cardInfo} = this.props
     return (
-			<div>
-				<Navbar crumbs={crumbs}/>
-				<article className="container">
-					{console.log('props', this.props)}
-					{dirs[0] 		?	<Directories	dirs={dirs}/>			:	null}
-					{files[0]		?	<Files 				files={files}/>		:	null}
+			<div id="outputer">
+				<nav>
+					<Navbar containerSize={containerSize} cardSize={cardSize} cardInfo={cardInfo}/>
+					<div className={'container ' + containerSize}>
+						{crumbs && crumbs[0] 	?	<Breadcrumbs	crumbs={crumbs}/>	:	null}
+					</div>
+				</nav>
+				<article className={'container ' + containerSize}>
+					{dirs && dirs[0] ?	<Directories dirs={dirs}/> : null}
+					{files && files[0] ? <Files files={files} cardSize={cardSize} cardInfo={cardInfo}/> : null}
 				</article>
 			</div>
     )
@@ -80,10 +96,10 @@ const Directories = React.createClass({
 // Displays the files in the current page, in Cards
 const Files = React.createClass({
   render: function() {
-		let {files} = this.props
+		let {files, cardSize, cardInfo} = this.props
     return (
-			<main className="files mt-1">
-				{files.map((file, i)=><Card file={file} key={i}/>)}
+			<main className="ot-files mt-1">
+				{files.map((file, i)=><Card file={file} key={i} cardSize={cardSize} cardInfo={cardInfo}/>)}
 			</main>
     )
   }
@@ -92,12 +108,14 @@ const Files = React.createClass({
 // Displays a file as a linked images
 const Card = React.createClass({
   render: function() {
-		let {file} = this.props
+		let {file, cardSize, cardInfo} = this.props
     return (
-			<div className="card">
-				<div className="card-block hidden-xs-up">
-			    <h4 className="card-title">{file.sequence}</h4>
-			  </div>
+			<div className={'card ' + cardSize}>
+				{cardInfo ?
+					<div className="card-block">
+						<h4 className="card-title">{file.sequence}</h4>
+					</div>
+				: null}
 				<div className="ot-imageWrapper">
 					<a target="_blank" href={file.URL}>
 						<img className="card-img-top img-fluid" src={file.URL} />
@@ -106,11 +124,11 @@ const Card = React.createClass({
 						<i className="fa fa-link"></i>
 					</button>
 				</div>
-				<ul className="list-group list-group-flush hidden-xs-up">
-					{file.keywords ?
-						file.keywords.map((keyword, i)=><li className="list-group-item" key={i}>{keyword}</li>)
-					: null}
+				{cardInfo && file.keywords ?
+				<ul className="list-group list-group-flush">
+					{file.keywords.map((keyword, i)=><li className="list-group-item" key={i}>{keyword}</li>)}
 			  </ul>
+				: null}
 			</div>
     )
   }
@@ -119,40 +137,111 @@ const Card = React.createClass({
 // Displays the Navbar
 // Its functionality will be developed in next commits. Right now just the breadcrumbs are dynamic
 const Navbar = React.createClass({
+	_changeContainerSize: function(containerSize, scale) {
+		let sizes = ['container--500', 'container--900', 'container--1140', 'container--full']
+		let currentSize = sizes.indexOf(containerSize)
+		let finalSize = containerSize
+		if (scale==="increase" && currentSize < sizes.length-1) {
+			finalSize= sizes[sizes.indexOf(containerSize)+1]
+		} else if (scale==="decrease"  && currentSize > 0) {
+			finalSize= sizes[sizes.indexOf(containerSize)-1]
+		}
+		setState({"containerSize": finalSize})
+		localStorage.setItem('containerSize', finalSize)
+	},
+	_changeCardSize: function(cardSize, scale) {
+		let sizes = ['card--80', 'card--120', 'card--180', 'card--full']
+		let currentSize = sizes.indexOf(cardSize)
+		let finalSize = cardSize
+		if (scale==="increase" && currentSize < sizes.length-1) {
+			finalSize = sizes[sizes.indexOf(cardSize)+1]
+		} else if (scale==="decrease"  && currentSize > 0) {
+			finalSize = sizes[sizes.indexOf(cardSize)-1]
+		}
+		setState({"cardSize": finalSize})
+		localStorage.setItem('cardSize', finalSize)
+	},
+	_changeCardInfo: function(cardInfo) {
+		setState({"cardInfo":!cardInfo})
+	},
+	_search: function(kind, input){
+		switch(kind) {
+			case "including":
+				setState({"searchIncluding": input})
+				break
+			case "excluding":
+				setState({"searchExcluding": input})
+				break
+		}
+		setTimeout(()=>{
+			if(state.searchIncluding) {
+				if(!state.searchExcluding) {
+					setState({"filesFiltered": lookFor(state.searchIncluding, state[state.cwd].files)})
+				} else {
+					setState({"filesFiltered": except(state.searchExcluding, lookFor(state.searchIncluding, state[state.cwd].files))})
+				}
+			} else if (!state.searchIncluding) {
+				setState({"filesFiltered": state[state.cwd].files})
+			}
+		}, 200)
+		function lookFor(term, files) {
+			let re = new RegExp(term, "i")
+			let filesFiltered = files
+				.filter(file=>file.keywords
+					.map(keyword=>re.test(keyword))
+					.reduce((a,b)=>a+b)
+				)
+			return filesFiltered
+		}
+		function except(term, files) {
+			let re = new RegExp(term, "i")
+			let filesFiltered = files
+				.filter(file=>!file.keywords
+						.map(keyword=>re.test(keyword))
+						.reduce((a,b)=>a+b)
+				)
+			return filesFiltered
+		}
+	},
   render: function() {
-		let {crumbs} = this.props
+		let {containerSize, cardSize, cardInfo} = this.props
     return (
-			<nav>
-				<div className="navbar">
-					<div className="container">
-						<button className="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
-						<div className="collapse navbar-toggleable-sm" id="navbarResponsive">
+			<div className="navbar">
+				<div className={'container ' + containerSize}>
+					<button className="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
+					<div className="collapse navbar-toggleable-sm" id="navbarResponsive">
 
-							<div className="btn-group ot-images" role="group" aria-label="Resize images">
-								<button type="button" className="btn btn-secondary"><i className="fa fa-compress" aria-hidden="true"></i></button>
-								<button type="button" className="btn btn-secondary"><i className="fa fa-expand" aria-hidden="true"></i></button>
-							</div>
-
-							<div className="btn-group ot-container" role="group" aria-label="Resize container">
-								<button type="button" className="btn btn-secondary"><i className="fa fa-mobile" aria-hidden="true"></i></button>
-								<button type="button" className="btn btn-secondary"><i className="fa fa-desktop" aria-hidden="true"></i></button>
-							</div>
-
-							<div className="btn-group ot-code" role="group" aria-label="Resize container">
-								<button type="button" className="btn btn-secondary"><i className="fa fa-code" aria-hidden="true"></i></button>
-							</div>
-
-							<form className="form-inline float-md-right">
-								<input className="ot-input form-control" type="text" placeholder="Search" />
-								<input className="ot-input form-control" type="text" placeholder="Exclude" />
-							</form>
+						<div className="btn-group ot-images" role="group" aria-label="Resize images">
+							<button type="button" className="btn btn-secondary" onClick={this._changeCardSize.bind(this, cardSize, "decrease")}>
+								<i className="fa fa-compress" aria-hidden="true"></i>
+							</button>
+							<button type="button" className="btn btn-secondary" onClick={this._changeCardSize.bind(this, cardSize, "increase")}>
+								<i className="fa fa-expand" aria-hidden="true"></i>
+							</button>
 						</div>
+
+						<div className="btn-group ot-container" role="group" aria-label="Resize container">
+							<button type="button" className="btn btn-secondary" onClick={this._changeContainerSize.bind(this, containerSize, "decrease")}>
+								<i className="fa fa-mobile" aria-hidden="true"></i>
+							</button>
+							<button type="button" className="btn btn-secondary" onClick={this._changeContainerSize.bind(this, containerSize, "increase")}>
+								<i className="fa fa-desktop" aria-hidden="true"></i>
+							</button>
+						</div>
+
+						<div className="btn-group ot-code" role="group" aria-label="Resize container">
+							<button type="button" className="btn btn-secondary" onClick={this._changeCardInfo.bind(this, cardInfo)}>
+								<i className="fa fa-code" aria-hidden="true"></i>
+							</button>
+						</div>
+
+						<form className="form-inline float-md-right">
+							<input className="ot-input form-control" type="text" placeholder="Search" onChange={e=>this._search("including", e.target.value)}/>
+							<input className="ot-input form-control" type="text" placeholder="Exclude" onChange={e=>this._search("excluding", e.target.value)} disabled={!state.searchIncluding}/>
+						</form>
 					</div>
 				</div>
-				<div className="container">
-					{crumbs[0] 	?	<Breadcrumbs	crumbs={crumbs}/>	:	null}
-				</div>
-			</nav>
+			</div>
 		)
 	}
 })
